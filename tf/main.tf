1df9d32114d4e76de279cd54ee93d30625b87486
Robin Mollah <robinsajin@gmail.com>
2022-11-13 04:38:10 +0600

    feat: Terraform create a GCP Bucket

---
+++ tf/main.tf
@@ -0,0 +1,7 @@
+
+resource "google_storage_bucket" "default" {
+  name = var.bucket_name
+  storage_class = var.storage_class
+  location = var.bucket_location
+  project = var.project_id
+}
---
+++ tf/variables.tf
@@ -0,0 +1,17 @@
+variable "bucket_name" {
+  default = "doshguti"
+}
+
+variable "storage_class" {
+  description = "any of STANDARD, NEARLINE, COLDLINE, ARCHIVE"
+  default = "STANDARD"
+}
+
+variable "bucket_location" {
+  description = "e.g.  ASIA-SOUTH2"
+  default = "ASIA-SOUTH2"
+}
+
+variable "project_id" {
+  description = "e.g. robinsajin"
+}
