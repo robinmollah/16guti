5fc2a89db31704af2273384ae95ae9b7762c61cd
robinmollah <robinsajin@gmail.com>
2022-12-05 20:43:14 +0000

    Sudo added in the run.sh file

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-docker stop 10guti
-docker rm 10guti
-docker build . -t 10guti
-docker run -p 3050:3050 --name 10guti 10guti
+sudo docker stop 10guti
+sudo docker rm 10guti
+sudo docker build . -t 10guti
+sudo docker run -d -p 3050:3050 --name 10guti 10guti
